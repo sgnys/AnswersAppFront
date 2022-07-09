e1a1c84d8182b59bbc5e010259466f35b25af678
Sławomir Gnyś <49480725+sgnys@users.noreply.github.com>
2022-07-09 08:45:19 +0200

    Add searching answers

--- src/components/AnswersComponents/AnswersList/AnswersList.tsx
+++ src/components/AnswersComponents/AnswersList/AnswersList.tsx
@@ -1,26 +1,23 @@
 import React, {useEffect, useState} from "react";
-import {AnswerGroupEnum, ListAnswersRes} from 'types';
+import {AnswerEntity, AnswerGroupEnum, ListAnswersRes} from 'types';
 import {Spinner} from "../../../common/Spinner";
 import {AnswerContent} from "../AnswerContent/AnswerContent";
-
 import {refreshLists} from "../../../utils/refreshLists";
 import {useLocation} from "react-router-dom";
-
 import {SortAnswers} from "../SortAnswers/SortAnswers";
 
 import './AnswersList.css';
 
 export const AnswersList = () => {
     const [data, setData] = useState<ListAnswersRes | null>(null);
-    const [category, setCategory] = useState('all')
+    const [category, setCategory] = useState('all');
+    const [searchText, setSearch] = useState('');
     let location = useLocation();
-
-
 
     useEffect(() => {
         if (category === AnswerGroupEnum.ALL) {
             refreshLists(location.pathname, setData);
-        }else{
+        } else {
             refreshLists(`/answers/sort/${category}`, setData);
         }
     }, [category])
@@ -28,16 +25,37 @@ export const AnswersList = () => {
 
     if (data === null) return <Spinner/>
 
+    const filterTasks: AnswerEntity[] = [...data.answersList].filter(answer => {
+        if (searchText === "") {
+            return answer
+        } else if (answer.text.toLowerCase().includes(searchText.toLowerCase())) {
+            return answer
+        }
+    });
+
+
     return (
         <>
             <section className='answers-sort'>
-             <SortAnswers category={category} onSetCategoryChange={setCategory}/>
+                <SortAnswers category={category} onSetCategoryChange={setCategory}/>
+            </section>
 
+            <section className='answers-search'>
+                <label htmlFor="search">
+                    Wyszukaj:
+                    <input
+                        type='text'
+                        id='search'
+                        placeholder='Wyszukaj'
+                        value={searchText}
+                        onChange={(e)=> setSearch(e.target.value)}
+                    />
+                </label>
             </section>
 
             <section className="answers-list">
                 <AnswerContent
-                    answersList={data.answersList}
+                    answersList={filterTasks}
                     templatesList={data.templatesList}
                     onAnswersListChange={() => refreshLists(location.pathname, setData)}/>
             </section>
